43791e963f18cc66f38427c79fd0ecd48f603f5b
Eason513 <66970275+Eason513@users.noreply.github.com>
2020-06-16 13:07:28 -0400

    Add files via upload

---
+++ work/hello.py
@@ -0,0 +1,18 @@
+print("hello world")
+#name=input("best country in the world:")
+#print(name)
+'''
+if (name.lower()=="china"):
+    print(name + "is thw best!")
+elif(name.lower()=="canada"):
+    print(name + "is the secomd best!")
+else:
+    print("wrong answer!")
+'''
+
+fruits=['apple','orange','banana','peach']
+for fruit in fruits:
+    templist=fruit.split(",")
+    print(fruit)
+    print(templist)
+    
